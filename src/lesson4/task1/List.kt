6376a53f7e06e272fe851a01f4b8aa05ed745532
Maksimeg <egorov.mo@edu.spbstu.ru>
2021-10-07 17:25:10 +0300

    very cool application

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -3,6 +3,7 @@
 package lesson4.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
 import kotlin.math.sqrt
 
 // Урок 4: списки
@@ -120,14 +121,32 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double {
+    var summa = 0.0
+    for (element in v){
+        summa = (summa + sqr(element))
+    }
+    return sqrt(summa)
+
+}
+
+
 
 /**
  * Простая (2 балла)
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    var k = 0
+    var summa = 0.0
+    for (element in list){
+        summa += element
+        k ++
+    }
+    if (summa == 0.0) return 0.0
+    return summa / k
+}
 
 /**
  * Средняя (3 балла)
