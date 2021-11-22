c7e520782387f0a71d6b307f5e85507fec7bb00a
Maksimeg <egorov.mo@edu.spbstu.ru>
2021-11-22 19:46:57 +0300

    very cool application

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -300,7 +300,17 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 4) -> Pair(0, 2)
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
-fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> = TODO()
+fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
+    val map = mutableMapOf<Int, Int>()
+    var k = 0
+    for (i in list.indices) {
+        if (list[i] in map)
+            return Pair(map[list[i]]!!, i)
+        k ++
+        map[number - k] = i
+    }
+    return Pair(-1, -1)
+}
 
 /**
  * Очень сложная (8 баллов)
