c536fa44597bb029dab18fa40bbf8c1ce968adfa
Maksimeg <egorov.mo@edu.spbstu.ru>
2022-02-25 13:53:05 +0300

    very cool application

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -2,11 +2,28 @@
 
 package lesson11.task1
 
+import lesson1.task1.sqr
+
+
 /**
  * Фабричный метод для создания комплексного числа из строки вида x+yi
  */
-fun Complex(s: String): Complex = TODO()
-
+fun Complex(s: String): Complex {
+    var str = s
+    val isReNegative = str.startsWith("-")
+    if (isReNegative) {
+        str = str.replaceFirst("-", "")
+    }
+    if (isReNegative && s.contains("+"))
+        return Complex(-str.split("+")[0].toDouble(), str.split("+")[1].removeSuffix("i").toDouble())
+    if (!isReNegative && s.contains("+"))
+        return Complex(str.split("+")[0].toDouble(), str.split("+")[1].removeSuffix("i").toDouble())
+    if (isReNegative && s.contains("-"))
+        return Complex(-str.split("-")[0].toDouble(), -str.split("-")[1].removeSuffix("i").toDouble())
+    if (!isReNegative && s.contains("-"))
+        return Complex(str.split("-")[0].toDouble(), -str.split("-")[1].removeSuffix("i").toDouble())
+    throw IllegalArgumentException("Wrong input syntax. Syntax should be: x+yi")
+}
 /**
  * Класс "комплексное число".
  *
@@ -21,40 +38,54 @@ class Complex(val re: Double, val im: Double) {
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x, 0.0)
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex): Complex = Complex(re + other.re, im + other.im)
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus(): Complex = Complex(-re, -im)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex = Complex(re - other.re, im - other.im)
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex = Complex(re * other.re - im * other.im, re * other.im + im * other.re)
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+    operator fun div(other: Complex): Complex = Complex(
+        (re * other.re + im * other.im) / (sqr(other.re) + sqr(other.im)),
+        (im * other.re - re * other.im) / (sqr(other.re) + sqr(other.im))
+    )
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is Complex && re == other.re && im == other.im
+
+    /**
+     * Хэш-код
+     */
+    override fun hashCode(): Int {
+        var result = 1
+        result = 31 * result + re.toInt()
+        result = 31 * result + im.toInt()
+        return result
+    }
+
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String = "${re}+${im}i"
 }
